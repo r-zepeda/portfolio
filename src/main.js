f9144893231424ba2db0f6992ee9372ba19beef6
r-zepeda <ralphpzepeda@gmail.com>
2024-01-07 15:28:32 +0800

    Index, Added Multiple Css

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 import '../src/styles/modern-normalize.css'
 import '../src/styles/style.css'
+import '../src/styles/desktop-navbar.css'
+import '../src/styles/mobile-navbar.css'
+import '../src/styles/mediaquery.css'
+import '../src/styles/utils.css'
+import '../src/styles/hero.css'
+import '../src/styles/about.css'
+import '../src/styles/techs.css'
+import '../src/scripts.js'
+
+
